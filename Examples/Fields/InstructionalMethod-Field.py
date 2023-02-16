44029d63a55cd875fd82ca6f4f751f4ec3a1e80a
jdchart <jacob.dchart@gmail.com>
2023-02-16 12:03:38 +0100

    InstructionalMethod fields

--- Examples/Fields/InstructionalMethod-Field.py
+++ Examples/Fields/InstructionalMethod-Field.py
@@ -31,6 +31,24 @@ print("The created web:")
 print(testWeb)
 print()
 
+# 2. Instructional method for a node
+nodeDisplay = {
+    "color" : "#67c781",
+    "important" : True, # For MemoRekall parsing, this will indicate if it is a top-level manifest or not
+    "annotationPaint" : True, # For MemoRekall parsing, indicate if the node is painted when it is an annotation
+    "annotationOverlay" : True, # For MemoRekall parsing, indicate if the annotation is painted ontop of the main ressource or underneath.
+    "annotationDisplayPos" : [0, 0], # For MemoRekall parsing, where to paint the ressourse on the canvas. (starting from top left if overlayed, starting from bottom of main ressource if not overlayed).
+    "annotationDisplayScale" : 1 # For MemoRekall parsing, the scale of the ressource (1 = full size)
+}
+testNode = testWeb.addNode({"title" : "My display node", "instructionalMethod" : nodeDisplay})
+
+# Set attributes:
+testNode.instructionalMethod.annotationOverlay = False
+
+print("Created node:")
+print(testNode)
+print()
+
 # Cleanup
 if cleanUp == True:
     if os.path.exists(testWeb.path):
--- spider/spider/InstructionalMethod.py
+++ spider/spider/InstructionalMethod.py
@@ -4,6 +4,10 @@ class InstructionalMethod:
     def __init__(self, **kwargs):
         self.color = kwargs.get('color', None)
         self.important = kwargs.get('important', None)
+        self.annotationPaint = kwargs.get('annotationPaint', None)
+        self.annotationOverlay = kwargs.get('annotationOverlay', None)
+        self.annotationDisplayPos = kwargs.get('annotationDisplayPos', None)
+        self.annotationDisplayScale = kwargs.get('annotationDisplayScale', None)
 
         if kwargs.get('from_string', None) != None:
             self.fromString(kwargs.get('from_string'))
@@ -14,7 +18,11 @@ class InstructionalMethod:
     def collectData(self):
         return {
             "color" : self.color,
-            "important" : self.important
+            "important" : self.important,
+            "annotationPaint" : self.annotationPaint,
+            "annotationOverlay" : self.annotationOverlay,
+            "annotationDisplayPos" : self.annotationDisplayPos,
+            "annotationDisplayScale" : self.annotationDisplayScale
         }
 
     def toString(self):
@@ -23,11 +31,19 @@ class InstructionalMethod:
             returnString = returnString + "&color=" + self.color
         if self.important != None:
             returnString = returnString + "&important=" + str(self.important)
+        if self.annotationPaint != None:
+            returnString = returnString + "&annotationPaint=" + str(self.annotationPaint)
+        if self.annotationOverlay != None:
+            returnString = returnString + "&annotationOverlay=" + str(self.annotationOverlay)
+        if self.annotationDisplayPos != None:
+            returnString = returnString + "&annotationDisplayPos=" + str(self.annotationDisplayPos[0]) + "," + str(self.annotationDisplayPos[1])
+        if self.annotationDisplayScale != None:
+            returnString = returnString + "&annotationDisplayScale=" + str(self.annotationDisplayScale)
         return returnString
 
     def fromString(self, stringIn):
         mainSplit = stringKeySplit(
-            ["&color=", "&important="], 
+            ["&color=", "&important=", "&annotationPaint=", "&annotationOverlay=", "&annotationDisplayPos=", "&annotationDisplayScale="], 
             stringIn
         )
         for i in range(len(mainSplit)):
@@ -38,3 +54,18 @@ class InstructionalMethod:
                     self.important = True
                 elif mainSplit[i + 1] == "False":
                     self.important = False
+            if mainSplit[i] == "&annotationPaint=":
+                if mainSplit[i + 1] == "True":
+                    self.annotationPaint = True
+                elif mainSplit[i + 1] == "False":
+                    self.annotationPaint = False
+            if mainSplit[i] == "&annotationOverlay=":
+                if mainSplit[i + 1] == "True":
+                    self.annotationOverlay = True
+                elif mainSplit[i + 1] == "False":
+                    self.annotationOverlay = False
+            if mainSplit[i] == "&annotationDisplayPos=":
+                posSplit = mainSplit[i + 1].split(",")
+                self.annotationDisplayPos = [int(posSplit[0]), int(posSplit[1])]
+            if mainSplit[i] == "&annotationDisplayScale=":
+                self.color = float(mainSplit[i + 1])
