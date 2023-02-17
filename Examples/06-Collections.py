5ba174f15dfbe7968f820dad0a6f8cc9361f1bef
jdchart <jacob.dchart@gmail.com>
2023-02-17 16:11:04 +0100

    Collection class

---
+++ Examples/06-Collections.py
@@ -0,0 +1,68 @@
+''' ================================================================================
+06 COLLECTIONS
+You can create collections of nodes and edges.
+This means that you can have several configurations of the web data
+without having to keep duplicates of the same data.
+We create collections of nodes and of edges so that they can grow independently.
+================================================================================ '''
+
+import spider as sp
+import os
+import shutil
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# 0. Script settings. 
+# Set this to true to remove these test directories once you're done.
+cleanUp = True
+
+# 1. Create a web with some content:
+web = sp.createWeb({"path" : "/Users/jacob/Documents/Git Repos/Spider Webs/Collections Test", "title" : "Test web"})
+nodeList = []
+for i in range(10):
+    nodeList.append(str(web.addNode({"title" : "Node " + str(i)}).uuid))
+edgeList = []
+for i in range(len(nodeList) - 1):
+    edgeList.append(str(web.addEdge({"title" : "Edge " + str(i + 1), "relation" : {"source" : nodeList[i], "target" : nodeList[i + 1]}}).uuid))
+
+# 2. Create a collection of nodes:
+myNodeCollection = web.addCollection("node", {"title" : "My node collection"})
+print("Created collection:")
+print(myNodeCollection)
+print()
+
+# (You can load collections in the same way as nodes and edges):
+loadedCollection = web.loadCollection(str(myNodeCollection.uuid))
+print("Loaded collection:")
+print(loadedCollection)
+print()
+
+# 3. We can add items to the collection using the addContent() function.
+# Either provide a uuid string, or an array of uuid strings
+myNodeCollection.addContent(nodeList)
+
+# 4. Lets create a collection of edges using all the edges, and another using only one:
+myEdgeCollection = web.addCollection("edge", {})
+myEdgeCollection.addContent(edgeList)
+
+mySecondEdgeCollection = web.addCollection("edge", {})
+mySecondEdgeCollection.addContent(edgeList[0])
+
+# 4. Use collections for things like network visualisation:
+# We make one using the first edge collection, and another using the second
+network = web.convertToNetwork(nodeList = myNodeCollection, edgeList = myEdgeCollection)
+network2 = web.convertToNetwork(nodeList = myNodeCollection, edgeList = mySecondEdgeCollection)
+
+# Visualise the collections:
+pos = nx.spring_layout(network, seed=3068)
+nx.draw(network, pos=pos, with_labels=True)
+plt.show()
+
+pos = nx.spring_layout(network2, seed=3068)
+nx.draw(network2, pos=pos, with_labels=True)
+plt.show()
+
+# Cleanup
+if cleanUp == True:
+    if os.path.exists(web.path):
+        shutil.rmtree(web.path)
---
+++ spider/spider/Collection.py
@@ -0,0 +1,53 @@
+import os
+from .SpiderElement import *
+from .utils import *
+
+class Collection(SpiderElement):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.parentPath = kwargs.get('parentPath', None)
+
+        if kwargs.get('read_from_file', None) == None:
+            if self.parentPath != None:
+                self.path = self.setPath(self.parentPath, kwargs.get('collection_type', "node"))
+                setattr(self, "identifier", self.path)
+
+        if kwargs.get('read_from_file', None) != None:
+            self.read(kwargs.get('read_from_file'))
+        
+        self.write()
+    
+    def __str__(self):
+        return super().__str__()
+
+    def write(self):
+        writeJson(self.collectData(), os.path.join(self.path, "collection.json"))
+        writeJson({"items" : []}, os.path.join(self.path, "content.json"))
+
+    def read(self, path):
+        readData = readJson(path)
+        super().setFromReadData(readData)
+
+    def setPath(self, path, collectiontype):
+        folderName = "node_collections/"
+        if collectiontype == "edge":
+            folderName = "edge_collections/"
+        makeDirsRecustive([
+            os.path.join(path, folderName + str(self.uuid))
+        ])
+        return os.path.join(path, folderName + str(self.uuid))
+
+    def addContent(self, toAdd):
+        contentData = readJson(os.path.join(self.path, "content.json"))
+        if isinstance(toAdd, str):
+            if toAdd not in contentData["items"]:
+                contentData["items"].append(toAdd)
+        elif isinstance(toAdd, list):
+            for item in toAdd:
+                if item not in contentData["items"]:
+                    contentData["items"].append(item)
+        writeJson(contentData, os.path.join(self.path, "content.json"))
+
+    def contentToList(self):
+        return readJson(os.path.join(self.path, "content.json"))["items"]
